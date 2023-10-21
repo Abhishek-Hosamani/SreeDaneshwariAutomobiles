21340dbb72ac1717447d1db675d6eca27be26e4d
Abhishek-Hosamani <01fe20bcs088@kletech.ac.in>
2023-10-21 22:23:08 +0530

    Login Page Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import { Home } from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
 function App() {
   return (
     <>
       <Home />
+
     </>
   );
 }
