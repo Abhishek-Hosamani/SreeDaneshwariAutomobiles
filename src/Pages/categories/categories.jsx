7737e5c08fd372db849da1595219cc98e05e6501
shashank-hegde-9471 <01fe20bcs068@kletech.ac.in>
2023-10-21 20:42:14 +0530

    first

---
+++ src/Pages/categories/categories.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Header } from '../../Components/Header'
+import TrolleyImg from '../../Assets/trolley Spares.png'
+import Tubeimg from '../../Assets/TubelessTyre.png'
+import CategoryCard from '../../Components/CategoryCard'
+import Carousel from '../../Components/Carousel'
+import Footer from '../../Components/Footer'
