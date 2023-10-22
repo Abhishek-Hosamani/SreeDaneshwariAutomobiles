4214235b28731abe861c3703bcc81a6e925e91e4
shashank-hegde-9471 <01fe20bcs068@kletech.ac.in>
2023-10-22 23:39:34 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import { Home } from './Pages/Home/Home';
-<<<<<<< HEAD
 import ItemCard from './Components/ItemCard';
 import { Categories } from './Pages/Categories/Categories';
-=======
 import Login from './Pages/Login/Login';
->>>>>>> 8096f86406cc0972bbcdfde723cbccfe9378c84c
 function App() {
   return (
     <>
